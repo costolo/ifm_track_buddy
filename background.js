de30243dc7e8720e7bcbced75b70ffe563cae5e4
Ben <ben.costolo@gmail.com>
2017-02-27 16:50:25 -0500

    append new track into correct container and add correct ID

--- background.js
+++ background.js
@@ -1,19 +1,20 @@
 window.chrome.runtime.onMessage.addListener((msg, sender) => {
   if (!msg.initialTracks) {
-    //appendTrack(msg.indvTrack, msg.channel)
+    appendTrack(msg.indvTrack, msg.channel)
   } else {
     window.chrome.pageAction.show(sender.tab.id)
   }
 })
 
 const appendTrack = (track, node) => {
-  const targetNode = document.getElementById(node)
+  const targetNode = document.getElementById(node).lastChild.querySelector('.dateContainer')
+  const newTrackID = parseInt(targetNode.lastChild.dataset.id, 10) + 1
   const component = backgroundTrackComponent(track)
+  component.dataset.id = newTrackID
   targetNode.appendChild(component)
 }
 
 const backgroundTrackComponent = (track) => {
-  console.log(track)
   const dateTimeArr = track.time.split('T')
   const container = document.createElement('li')
   const component = `<a href="https://www.google.com/#q=${track.track}">${track.track} @ ${dateTimeArr[1]}</a>`
